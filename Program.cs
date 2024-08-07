2e74572b6c3bdb91a9b135c44aa4253649a3b7b8
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-07 20:57:43 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            double weight;
+            double height;
+            Console.WriteLine("Please enter a height");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a weight");
+            weight = Convert.ToDouble(Console.ReadLine());
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+            Console.WriteLine("BMI: " + bmi);
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("");
+            }
+            else if (bmi < 25.0)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (bmi < 30.0)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Obese");
+            }
+        }
+    }
+}
